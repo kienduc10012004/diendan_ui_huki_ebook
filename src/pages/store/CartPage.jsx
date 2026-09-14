6af10f0100b5f1c7e59c731fe7272184b4a9846a
huynhgiahuy236 <huynhgiahuy236@gmail.com>
2026-09-14 23:36:17 +0700

    feat: add CartPage component for managing shopping cart and checkout options

--- src/pages/store/CartPage.jsx
+++ src/pages/store/CartPage.jsx
@@ -26,7 +26,7 @@ export default function CartPage() {
   const [savedItems, setSavedItems] = useState([
     {
       id: 'saved-1',
-      title: 'Dám Bị ',
+      title: ' Bị ',
       author: 'Koga Fumitake, Kishimi Ichiro',
       format: 'Ebook',
       price: 69000,
